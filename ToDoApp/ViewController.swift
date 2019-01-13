a5c82c63a5c28a8fb9986bfc91b091512ca69837
ogya 1 <ogya1@ogyas-MacBook-Pro.local>
2019-01-13 19:27:18 +0700

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by ogya 1 on 13/01/19.
+//  Copyright © 2019 ogya 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
